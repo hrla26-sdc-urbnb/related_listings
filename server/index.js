4099a6c62d890b775a917f583dc65971aa037f52
Sasha <sashguiab@gmail.com>
2019-01-03 10:51:26 -0800

    updated server to port 4000

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const db = require('../db/index.js');
 
-const port = 3000;
+const port = 4000;
 const server = http.createServer(app);
 
 server.listen(port, () => console.log('listening on port', port));
